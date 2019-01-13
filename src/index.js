fbc0607476b1dbf34736e6ced5019e6f985236ed
Alexandre Fontoura <xesfnet@gmail.com>
2019-01-13 14:06:38 +0000

    Get LBA Text Entries and randomly display them in the console.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import { loadTexts } from './text';
+import Languages from './constants';
 
-import { loadHqr } from './hqr';
+const language = Languages.LBA2[0];
+const texts = loadTexts(language, language.entries[Math.floor((Math.random() * language.entries.length))]);
 
-const textHqr = loadHqr('TEXT.HQR');
-console.info(textHqr.length);
-
+const text = texts[Math.floor(Math.random() * texts.length)];
+console.info(text.value);
---
+++ src/text.js
@@ -0,0 +1,43 @@
+import { loadHqr } from './hqr';
+
+function getLanguageTextIndex(language, index) {
+    const languageIndex = index + (30 * language.index);
+    return { data: language, index: languageIndex };
+}
+
+export function loadTextData(hqr, language) {
+    const mapData = new Uint16Array(hqr.getEntry(language.index));
+    const data = new DataView(hqr.getEntry(language.index + 1));
+    const texts = []; // {};
+    let start;
+    let end;
+    let idx = 0;
+
+    do {
+        start = data.getUint16(idx * 2, true);
+        end = data.getUint16((idx * 2) + 2, true);
+        const type = data.getUint8(start, true);
+        let value = '';
+        for (let i = start + 1; i < end - 1; i += 1) {
+            value += String.fromCharCode((language.data.charmap) ?
+                language.data.charmap[data.getUint8(i)]
+                : data.getUint8(i)
+            );
+        }
+        //texts[mapData[idx]] = {type, index: idx, value};
+        texts.push({type, index: idx, value});
+        idx += 1;
+    } while (end < data.byteLength);
+
+    return texts;
+}
+
+export function getTextFile(language) {
+    const fanSuffix = language.isFan ? `_${language.code}` : '';
+    return `TEXT2${fanSuffix}.HQR`;
+}
+
+export function loadTexts(language, index) {
+    const textHqr = loadHqr(getTextFile(language));
+    return loadTextData(textHqr, getLanguageTextIndex(language, index));
+}
