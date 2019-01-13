1add5036b32f55578c1c232960ea52a22fd3a046
Alexandre Fontoura <xesfnet@gmail.com>
2019-01-13 19:40:06 +0000

    Randonly send message to a channel every 3 hours and send random messages on request.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
-import Discord  from 'discord.js';
 
+import Discord  from 'discord.js';
 import { loadTexts } from './text';
 import Languages from './constants';
 import config from './config.json';
 
 const language = Languages.LBA2[0];
-const texts = loadTexts(language, language.entries[Math.floor((Math.random() * language.entries.length))]);
-
+/* const texts = loadTexts(language, language.entries[Math.floor((Math.random() * language.entries.length))]);
 const text = texts[Math.floor(Math.random() * texts.length)];
-console.info(text.value);
+console.info(text.value); */
+
+const randomText = () => {
+    const texts = loadTexts(language, language.entries[Math.floor((Math.random() * language.entries.length))]);
+    return texts[Math.floor(Math.random() * texts.length)];
+};
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+    console.log(`Logged in as ${client.user.tag}!`);
+    const channel = client.channels.get('534072057286361089') // #quote channel
+    setInterval(
+        () => {
+            const text = randomText();
+            const message = `${text.value}`;
+            const quote = '```' + message + '```' + `*\`LBA2 (#${text.index})\`*`;
+            channel.send(quote);
+        },
+        10800000 // every 3h
+    );
+})
 
-client.on('message', msg => {
-    console.info(msg);
-    
+client.on('message', message => {
     // ignore other bots or itself
     if (message.author.bot) {
        return;
@@ -34,7 +46,13 @@ client.on('message', msg => {
 
     switch(command) {
         case 'ping':
-            msg.reply('pong');
+            message.reply('pong');
+            break;
+        case 'lba2':
+            const text = randomText();
+            const message = `${text.value}`;
+            const quote = '```' + message + '```' + `*\`LBA2 (#${text.index})\`*`;
+            message.reply(quote);
             break;
     }
 });
